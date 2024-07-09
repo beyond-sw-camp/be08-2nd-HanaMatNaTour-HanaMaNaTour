e4995eef09234c388c68a72b14ac829220fa08dc
kanghana1 <qazplm12093@naver.com>
2024-07-09 20:43:57 +0900

    feat : 유저 도메인, 레포지토리 구1차 구현

--- src/main/java/com/example/demo/src/user/domain/User.java
+++ src/main/java/com/example/demo/src/user/domain/User.java
@@ -1,10 +1,19 @@
 package com.example.demo.src.user.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    private Long userId;
+    private String userName;
+    private String userNickname;
+    private String userEmail;
+    private String refreshToken;
 
 }
---
+++ src/main/java/com/example/demo/src/user/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.demo.src.user.service;
+
+public class UserService {
+
+
+
+
+}
