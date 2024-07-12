17035614fe060b59f44e51e163cb79b87558f479
kanghana1 <qazplm12093@naver.com>
2024-07-12 23:56:11 +0900

    feat: usreProfile service, dto 구현

---
+++ src/main/java/com/example/demo/src/user/dto/UserResponseDto.java
@@ -0,0 +1,33 @@
+package com.example.demo.src.user.dto;
+
+import com.example.demo.src.user.model.Authority;
+import lombok.Builder;
+import lombok.Data;
+
+public class UserResponseDto {
+
+
+    @Data
+    @Builder
+    public static class UserInfoResponseDto {
+        private String userName;
+        private String userNickname;
+        private String userEmail;
+        private Authority userAuthority;
+        private String refreshToken;
+    }
+
+
+    // 프로필 관련
+    @Data
+    @Builder
+    public static class userProfileResponseDto {
+        private String userName;
+        private String userNickname;
+        private String userEmail;
+        private int numOfReview;
+//        private List<Review> reviewList;
+        private int numOfList;
+//        private List<List> listList;
+    }
+}
