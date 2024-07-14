7a831cea81c075d15616d38a1aaceb05d6809dfa
kanghana1 <qazplm12093@naver.com>
2024-07-15 08:49:20 +0900

    feat: 리다이렉트 í html 페이지 만들기

--- src/main/java/com/example/demo/src/user/controller/UserController.java
+++ src/main/java/com/example/demo/src/user/controller/UserController.java
@@ -1,9 +1,21 @@
 package com.example.demo.src.user.controller;
 
+import com.example.demo.src.user.service.UserProfileService;
+import com.example.demo.src.user.service.UserSignUpAndFindService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserProfileService userProfileService;
+    private final UserSignUpAndFindService userSignUpAndFindService;
 
+    @GetMapping("/login")
+    public String login() { // 이건 그냥 소셜로그인 code받을라고 ..
+        return "login";
+    }
 }
