155e5bf054f79d4321cba4c61c3d25e02cbe0727
hoyeon96 <hoyen96@naver.com>
2024-07-15 00:18:50 +0900

    BoardMapper 작성 BoardService 틀잡기

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootBoardApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootBoardApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 }
