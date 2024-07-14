01fe942482b2a2e570455d0af4319dd7cf511824
kuk329 <kuk329@cau.ac.kr>
2024-07-15 05:37:09 +0900

    feat: BaseResponse 형식

--- src/main/java/com/example/demo/common/config/SecurityConfig.java
+++ src/main/java/com/example/demo/common/config/SecurityConfig.java
@@ -1,43 +1,43 @@
-package com.example.demo.common.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-
-        httpSecurity
-         //       .httpBasic(auth->auth.disable())
-                .csrf(auth->auth.disable());
-
-        // 인증 관련
-        httpSecurity
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/items/**","/users"
-                        ).permitAll()
-                        .anyRequest()
-                        .authenticated()
-                );
-
-        httpSecurity
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        return httpSecurity.build();
-
-    }
-}
+//package com.example.demo.common.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+//
+//        httpSecurity
+//         //       .httpBasic(auth->auth.disable())
+//                .csrf(auth->auth.disable());
+//
+//        // 인증 관련
+//        httpSecurity
+//                .authorizeHttpRequests((auth) -> auth
+//                        .requestMatchers("/items/**","/users"
+//                        ).permitAll()
+//                        .anyRequest()
+//                        .authenticated()
+//                );
+//
+//        httpSecurity
+//                .sessionManagement((session) -> session
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//
+//        return httpSecurity.build();
+//
+//    }
+//}
