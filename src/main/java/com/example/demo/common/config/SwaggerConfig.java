e89490d43435dc2fd37c895fab14696711bbb284
kuk329 <kuk329@cau.ac.kr>
2024-07-30 16:38:11 +0900

    docs: swagger에 jwt 토큰 전송을 위한 설정 추가

--- src/main/java/com/example/demo/common/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/common/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,9 +18,19 @@ import org.springframework.context.annotation.Configuration;
         )
 )
 public class SwaggerConfig {
+    SecurityScheme apiKey = new SecurityScheme()
+            .type(SecurityScheme.Type.APIKEY)
+            .in(SecurityScheme.In.HEADER)
+            .name("Authorization");
+
+    SecurityRequirement securityRequirement = new SecurityRequirement()
+            .addList("Bearer Token");
+
 
     @Bean
     public OpenAPI openAPI(){
-        return new OpenAPI().components(new Components());
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+                .addSecurityItem(securityRequirement);
     }
 }
